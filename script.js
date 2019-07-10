30aee683bfeb9a22a22112759e086c2fb94d3b5c
michaelthephoenix <nkono.mic@gmail.com>
2019-07-10 23:59:10 +0300

    user sign in

---
+++ script.js
@@ -0,0 +1,27 @@
+var signIn = document.getElementById('signInLink');
+var signUpForm = document.getElementById('signUpForm');
+var signInForm = document.getElementById('signInForm');
+
+function openSignInForm() {
+    signUpForm.style.display = 'none'
+    signInForm.style.display = 'block'
+
+    
+}
+
+function logIn() {
+    var userName = document.getElementById('uname').value;
+    var password = document.getElementById('psw').value;
+    
+    
+    if (userName == "user" && password == "user") {
+        window.open('user.html');
+        
+    }
+    if (userName == "admin" && password == "admin") {
+        window.open('admin.html')
+    }
+    else {
+        alert("Access Denied")
+    }
+}
